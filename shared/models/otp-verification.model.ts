5ce4c8bfc5b67525785e8e4b18dbffe9feddc115
Amir <amir@codeus.in>
2025-08-25 18:46:39 +0530

    otp-verification model changed a bit

--- shared/models/otp-verification.model.ts
+++ shared/models/otp-verification.model.ts
@@ -8,64 +8,48 @@ export interface IOtpVerification extends Document {
     updatedAt: Date;
 }
 
-// Put all instance methods in this interface
+// Instance methods
 export interface IOtpVerificationMethods {
-    generateOtp: () => string;
-
-    setExpiresAt: (OTP_EXPIRATION_MINUTES: any) => void;
+    generateOtp(): string;
+    setExpiresAt(OTP_EXPIRATION_MINUTES: number): void;
 }
 
-export const otpVerificationSchema = new Schema<IOtpVerification, OtpVerificationModel, IOtpVerificationMethods>(
+// Document type = schema fields + methods
+export type OtpVerificationDocument = IOtpVerification & IOtpVerificationMethods;
+
+// Static model type
+export interface OtpVerificationModel extends Model<OtpVerificationDocument> {}
+
+export const otpVerificationSchema = new Schema<OtpVerificationDocument, OtpVerificationModel>(
     {
-        phoneNumber: {
-            type: String,
-            required: true,
-        },
-        otp: {
-            type: String,
-        },
-        expiresAt: {
-            type: Date,
-        },
+        phoneNumber: { type: String, required: true },
+        otp: { type: String },
+        expiresAt: { type: Date },
     },
     {
         timestamps: true,
         versionKey: false,
         collection: 'OtpVerification',
-        toJSON: {
-            getters: true,
-        },
-        toObject: {
-            getters: true,
-        },
     }
 );
 
-otpVerificationSchema.methods.generateOtp = () => {
+otpVerificationSchema.methods.generateOtp = function () {
     const length = 4;
     let otp = '';
     const characters = '0123456789';
-    const charactersLength = characters.length;
 
     for (let i = 0; i < length; i++) {
-        otp += characters.charAt(Math.floor(Math.random() * charactersLength));
+        otp += characters.charAt(Math.floor(Math.random() * characters.length));
     }
-
-    // Pad the OTP with leading zeros if necessary
-    otp = otp.padStart(length, '0');
-    return otp;
+    return otp.padStart(length, '0');
 };
 
-otpVerificationSchema.methods.setExpiresAt = function (OTP_EXPIRATION_MINUTES) {
+otpVerificationSchema.methods.setExpiresAt = function (OTP_EXPIRATION_MINUTES: number) {
     const now = new Date();
-
     this.expiresAt = new Date(now.getTime() + OTP_EXPIRATION_MINUTES * 60000);
 };
 
-// Put all static methods in this interface
-interface OtpVerificationModel extends Model<IOtpVerification, {}, IOtpVerificationMethods> {}
-
-export const OtpVerification = mongoose.model<IOtpVerification, OtpVerificationModel>(
-    'OptVerification',
+export const OtpVerification = mongoose.model<OtpVerificationDocument, OtpVerificationModel>(
+    'OtpVerification',
     otpVerificationSchema
 );
